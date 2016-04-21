8878200ce39b42a1b4f91045f31501b49b29181c
Safiyat Reza <md.reza@snapdeal.com>
2016-04-22 01:06:06 +0530

    CLASSified the files.

--- copy.py
+++ copy.py
@@ -18,6 +18,55 @@ class Color:
     END = '\033[0m'
 
 
+class File(object):
+    def __init__(self, path=None):
+        if path is not None:
+            self.set_filename(path)
+
+    def set_filename(self, path):
+        self.abspath = os.path.abspath(path)
+        # self.relpath = self.abspath with head common with
+        #                os.path.abspath('.') removed
+        self.relpath = path
+        self.md5sum = self.md5(self.abspath)
+        self.size = os.path.getsize(self.abspath)
+        self.atime = os.path.getatime(self.abspath)
+        self.ctime = os.path.getctime(self.abspath)
+        self.mtime = os.path.getmtime(self.abspath)
+
+    def get_filename(self):
+        return self.abspath
+
+    def get_filesize(self):
+        return self.size
+
+    def get_fileatime(self):
+        return self.atime
+
+    def get_filectime(self):
+        return self.ctime
+
+    def get_filemtime(self):
+        return self.mtime
+
+    def get_filetime(self):
+        return (self.get_fileatime(), self.get_filectime(),
+                self.get_filemtime())
+
+    def get_md5(self):
+        return self.md5sum.hexdigest()
+
+    def md5(self, path):
+        md5object = hashlib.new('md5')
+        BLOCKSIZE = 4096
+        f = open(path)
+        text = f.read(BLOCKSIZE)
+        while text:
+            md5object.update(text)
+            text = f.read(BLOCKSIZE)
+        return md5object
+
+
 def list_contents(path='.', full_list=None):
     path = os.path.abspath(path)
     contents = os.listdir(path)
@@ -35,26 +84,15 @@ def list_contents(path='.', full_list=None):
     return full_list
 
 
-def md5(path):
-    md5object = hashlib.new('md5')
-    BLOCKSIZE = 4096
-    path = os.path.abspath(path)
-    f = open(path)
-    text = f.read(BLOCKSIZE)
-    while text:
-        md5object.update(text)
-        text = f.read(BLOCKSIZE)
-    return md5object.hexdigest()
-
-
 def main():
     path = sys.argv[1]
 
     file_list = list_contents(path)
 
     for f in file_list:
-        md5sum = md5(f)
-        print "%s : %s" % (f.ljust(50), md5sum.ljust(60))
+        fileobj = File(f)
+        print "%s : %s" % (fileobj.get_filename().ljust(50),
+                           fileobj.get_md5().ljust(60))
 
 
 if __name__ == '__main__':
